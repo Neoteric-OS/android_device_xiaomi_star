576f4470e98f9c84d490a154f0d17662628e2a5b
Akash Srivastava <akashniki@gmail.com>
2022-06-12 01:15:42 +0530

    star: Drop HIDL packages
    * These are built by default in CAF

--- lahaina.mk
+++ lahaina.mk
@@ -238,10 +238,6 @@ PRODUCT_PACKAGES += \
     android.hidl.memory.block@1.0 \
     android.hidl.memory@1.0-impl
 
-PRODUCT_PACKAGES += \
-    libhidltransport.vendor \
-    libhwbinder.vendor
-
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
