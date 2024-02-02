4cb2c30f12992c18ab4e08826eb696879ffc9bca
Akash Srivastava <akashniki@gmail.com>
2024-02-02 13:45:51 +0100

    star: Switch to qti WLAN component

--- lahaina.mk
+++ lahaina.mk
@@ -292,6 +292,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     nfc \
     perf \
     telephony \
+    wlan \
     usb
 
 # Radio
@@ -377,14 +378,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi-service \
-    hostapd \
-    libwifi-hal-qcom \
-    libwpa_client \
-    WifiOverlay \
-    wpa_cli \
-    wpa_supplicant \
-    wpa_supplicant.conf
+    WifiOverlay
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
