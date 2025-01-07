fb2104d9f84921a8db38adac34f690b9f3b321f0
Michael Bestas <mkbestas@lineageos.org>
2025-01-07 01:36:36 +0100

    star: Switch to AIDL thermal HAL
    Change-Id: I6ae621194044ab66fad69b9ef997b3b347bf6120

--- lahaina.mk
+++ lahaina.mk
@@ -351,7 +351,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0 \
     android.hardware.thermal@2.0.vendor \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal-service.qti
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
